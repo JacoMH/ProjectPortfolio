ac3e3d4a57e65e94482ee13763dbb068428c4401
Jacob Heyes <heyesjacob@gmail.com>
2024-10-15 17:45:38 +0100

    gif-maker page done

--- resources/views/projects/gif-maker.blade.php
+++ resources/views/projects/gif-maker.blade.php
@@ -15,9 +15,44 @@
 
        <!-- description of features -->
        <div class='text-3xl text-center text-blue-500 mb-12 mt-[60px]'>Feature Breakdown</div>
-    <div>
-        <p></p>
-    </div>
+    <section class='flex flex-col'>
+        <div class='flex justify-center'>
+            <!-- description -->
+            <p class='mr-[200px] container max-w-[500px] text-left text-lg leading-relaxed text-gray-700 content-center'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo ex necessitatibus ut ipsam aliquam aspernatur mollitia, fuga beatae id illo perferendis voluptates incidunt ullam accusantium non! Modi vitae odit eveniet?
+            </p>
+
+            <div>
+                <img class=' ml-auto mr-10 mb-10 my-3 max-w-[450px] rounded-lg shadow-lg ring-4 ring-blue-500 ring-opacity-50 ring-offset-2 ring-offset-blue-200 hover:ring-opacity-100 hover:ring-offset-4 hover:scale-105 transition-all duration-300 ease-in-out' src="{{asset('gif-maker\order of images gif maker.gif')}}"></img>        
+            </div>
+        </div>
+
+        <div class='flex justify-center'>
+            <!-- image of UI -->
+            <div>
+                <img class=' mr-auto ml-10 mb-10 my-3 max-w-[450px] rounded-lg shadow-lg ring-4 ring-blue-500 ring-opacity-50 ring-offset-2 ring-offset-blue-200 hover:ring-opacity-100 hover:ring-offset-4 hover:scale-105 transition-all duration-300 ease-in-out' src="{{asset('gif-maker\delete image.gif')}}"></img>       
+                <img class=' mr-auto ml-10 mb-10 my-3 max-w-[450px] rounded-lg shadow-lg ring-4 ring-blue-500 ring-opacity-50 ring-offset-2 ring-offset-blue-200 hover:ring-opacity-100 hover:ring-offset-4 hover:scale-105 transition-all duration-300 ease-in-out' src="{{asset('gif-maker\delete image.gif')}}"></img>     <!-- want to change this so they overlap and when hover it brings to front -->   
+            </div>
+            
+    
+            <!-- description -->
+            <p class='ml-[200px] container max-w-[500px] text-left text-lg leading-relaxed text-gray-700 content-center'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo ex necessitatibus ut ipsam aliquam aspernatur mollitia, fuga beatae id illo perferendis voluptates incidunt ullam accusantium non! Modi vitae odit eveniet?
+            </p>
+        </div>
+
+
+        <div class='flex justify-center'>
+            <!-- description -->
+            <p class='mr-[200px] container max-w-[500px] text-left text-lg leading-relaxed text-gray-700 content-center'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo ex necessitatibus ut ipsam aliquam aspernatur mollitia, fuga beatae id illo perferendis voluptates incidunt ullam accusantium non! Modi vitae odit eveniet?
+            </p>
+
+            <div>
+                <img class=' ml-auto mr-10 mb-10 my-3 max-w-[450px] rounded-lg shadow-lg ring-4 ring-blue-500 ring-opacity-50 ring-offset-2 ring-offset-blue-200 hover:ring-opacity-100 hover:ring-offset-4 hover:scale-105 transition-all duration-300 ease-in-out' src="{{asset('gif-maker\replace image.gif')}}"></img>        
+            </div>
+        </div>
+    </section>
 
 
     <!-- output -->
