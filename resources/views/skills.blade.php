f9dcc33db50dcd6132b9b183ea9d7b8095825a03
Jacob Heyes <heyesjacob@gmail.com>
2024-10-21 01:31:53 +0100

    first page semi-done, need to do the writing for the projects

--- resources/views/skills.blade.php
+++ resources/views/skills.blade.php
@@ -8,7 +8,7 @@
                         </svg>
                     <h3 class="ml-4 text-2xl font-semibold">HTML</h3>
                 </div>
-                <p class="mt-4 text-gray-700 text-sm leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consequat, justo ac vehicula fringilla, risus nunc auctor arcu, a gravida sapien quam vitae sapien.</p>
+                <p class="mt-4 text-gray-700 text-sm leading-relaxed">HTML is used in all of my web based applications. Over my time in first year I have become familiar with how to properly format HTML to display page information as I intend without sacrificing readability of the code i.e. DIV Soup by using appropriate tags.</p>
             </div>
     
             <div class='max-w-[350px] m-5 rounded-lg'>
@@ -18,7 +18,7 @@
                         </svg>
                     <h3 class="ml-4 text-2xl font-semibold">CSS</h3>
                 </div>
-                <p class="mt-4 text-gray-700 text-sm leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras suscipit arcu id turpis auctor, et vulputate justo efficitur. Integer volutpat, justo nec pharetra mollis.</p>
+                <p class="mt-4 text-gray-700 text-sm leading-relaxed">Along with HTML my developing HTML skill, this complements my ability to use CSS effectively. Using raw CSS has taught me the fundamentals of web-based design and the properties required to stylise a web page appropriately.</p>
             </div>
     
             <div class='max-w-[350px] m-5 rounded-lg'>
@@ -28,7 +28,7 @@
                         </svg>
                     <h3 class="ml-4 text-2xl font-semibold">JavaScript</h3>
                 </div>
-                <p class="mt-4 text-gray-700 text-sm leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vehicula libero sit amet elit hendrerit, ac ullamcorper felis faucibus. Donec fringilla ligula at libero efficitur scelerisque.</p>
+                <p class="mt-4 text-gray-700 text-sm leading-relaxed">JavaScript has been used in the development of my knowledge of Web Development later on. I used it in order to dynamically change information on a web based application without having to use backend e.g. PHP and therefore having to do constant page refreshes.</p>
             </div>
     </div>
     
@@ -40,7 +40,7 @@
                 </svg>
                 <h3 class="ml-4 text-2xl font-semibold">PHP</h3>
             </div>
-            <p class="mt-4 text-gray-700 text-sm leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum quis orci eget sapien cursus fermentum ut et metus. Aliquam erat volutpat.</p>
+            <p class="mt-4 text-gray-700 text-sm leading-relaxed">I have concentrated heavily in PHP in my first year of University and as a result I have developed a strong knowledge of the fundamentals of backend Web Development and how to effectively communicate with databases. I have therefore also been using this to create dynamic webpages in my websites.</p>
         </div>
         <div class='max-w-[350px] m-5 rounded-lg'>
             <div class="flex items-center">
@@ -48,7 +48,7 @@
                     <path d="M9,13.7q1.4-5.6,7-5.6c5.6,0,6.3,4.2,9.1,4.9q2.8.7,4.9-2.1-1.4,5.6-7,5.6c-5.6,0-6.3-4.2-9.1-4.9Q11.1,10.9,9,13.7ZM2,22.1q1.4-5.6,7-5.6c5.6,0,6.3,4.2,9.1,4.9q2.8.7,4.9-2.1-1.4,5.6-7,5.6c-5.6,0-6.3-4.2-9.1-4.9Q4.1,19.3,2,22.1Z" style="fill:#3f81f5"></path></g></svg>
                 <h3 class="ml-4 text-2xl font-semibold">TailwindCSS</h3>
             </div>
-            <p class="mt-4 text-gray-700 text-sm leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras suscipit arcu id turpis auctor, et vulputate justo efficitur. Integer volutpat, justo nec pharetra mollis.</p>
+            <p class="mt-4 text-gray-700 text-sm leading-relaxed">Building upon my knowledge of CSS and to avoid creating many CSS labels and having a large CSS file, I have decided to build upon my knowledge and use tailwindCSS. This Technology has allowed me to rapidly increase the speed of my workflow, and in the process make the webpages I create look more pleasing to the eye.</p>
         </div>
 
         <div class='max-w-[350px] m-5 rounded-lg'>
@@ -57,7 +57,7 @@
              <g> <path d="M255.855641,59.619717 C255.950565,59.9710596 256,60.3333149 256,60.6972536 L256,117.265345 C256,118.743206 255.209409,120.108149 253.927418,120.843385 L206.448786,148.178786 L206.448786,202.359798 C206.448786,203.834322 205.665123,205.195421 204.386515,205.937838 L105.27893,262.990563 C105.05208,263.119455 104.804608,263.201946 104.557135,263.289593 C104.464333,263.320527 104.376687,263.377239 104.278729,263.403017 C103.585929,263.58546 102.857701,263.58546 102.164901,263.403017 C102.051476,263.372083 101.948363,263.310215 101.840093,263.26897 C101.613244,263.186479 101.376082,263.1143 101.159544,262.990563 L2.07258227,205.937838 C0.7913718,205.201819 0,203.837372 0,202.359798 L0,32.6555248 C0,32.2843161 0.0515567729,31.9234187 0.144358964,31.5728326 C0.175293028,31.454252 0.24747251,31.3459828 0.288717928,31.2274022 C0.366053087,31.0108638 0.438232569,30.7891697 0.55165747,30.5880982 C0.628992629,30.4540506 0.742417529,30.3457814 0.83521972,30.2220451 C0.953800298,30.0570635 1.06206952,29.8869261 1.20127281,29.7425672 C1.31985339,29.6239866 1.4745237,29.5363401 1.60857131,29.4332265 C1.75808595,29.3094903 1.89213356,29.1754427 2.06227091,29.0774848 L2.06742659,29.0774848 L51.6134853,0.551122364 C52.8901903,-0.183535768 54.4613221,-0.183535768 55.7380271,0.551122364 L105.284086,29.0774848 L105.294397,29.0774848 C105.459379,29.1805983 105.598582,29.3094903 105.748097,29.4280708 C105.882144,29.5311844 106.031659,29.6239866 106.15024,29.7374115 C106.294599,29.8869261 106.397712,30.0570635 106.521448,30.2220451 C106.609095,30.3457814 106.727676,30.4540506 106.799855,30.5880982 C106.918436,30.7943253 106.985459,31.0108638 107.06795,31.2274022 C107.109196,31.3459828 107.181375,31.454252 107.212309,31.5779883 C107.307234,31.9293308 107.355765,32.2915861 107.356668,32.6555248 L107.356668,138.651094 L148.643332,114.878266 L148.643332,60.6920979 C148.643332,60.3312005 148.694889,59.9651474 148.787691,59.619717 C148.823781,59.4959808 148.890804,59.3877116 148.93205,59.269131 C149.014541,59.0525925 149.08672,58.8308984 149.200145,58.629827 C149.27748,58.4957794 149.390905,58.3875102 149.478552,58.2637739 C149.602288,58.0987922 149.705401,57.9286549 149.84976,57.7842959 C149.968341,57.6657153 150.117856,57.5780688 150.251903,57.4749553 C150.406573,57.351219 150.540621,57.2171714 150.705603,57.1192136 L150.710758,57.1192136 L200.261973,28.5928511 C201.538395,27.8571345 203.110093,27.8571345 204.386515,28.5928511 L253.932573,57.1192136 C254.107866,57.2223271 254.241914,57.351219 254.396584,57.4697996 C254.525476,57.5729132 254.674991,57.6657153 254.793572,57.7791402 C254.93793,57.9286549 255.041044,58.0987922 255.16478,58.2637739 C255.257582,58.3875102 255.371007,58.4957794 255.443187,58.629827 C255.561767,58.8308984 255.628791,59.0525925 255.711282,59.269131 C255.757683,59.3877116 255.824707,59.4959808 255.855641,59.619717 Z M247.740605,114.878266 L247.740605,67.8378666 L230.402062,77.8192579 L206.448786,91.6106946 L206.448786,138.651094 L247.745761,114.878266 L247.740605,114.878266 Z M198.194546,199.97272 L198.194546,152.901386 L174.633101,166.357704 L107.351512,204.757188 L107.351512,252.27191 L198.194546,199.97272 Z M8.25939501,39.7961379 L8.25939501,199.97272 L99.0921175,252.266755 L99.0921175,204.762344 L51.6392637,177.906421 L51.6237967,177.89611 L51.603174,177.885798 C51.443348,177.792996 51.3093004,177.658949 51.1597857,177.545524 C51.0308938,177.44241 50.8813791,177.359919 50.7679542,177.246494 L50.7576429,177.231027 C50.6235953,177.102135 50.5307931,176.942309 50.4173682,176.79795 C50.3142546,176.658747 50.1905184,176.540167 50.1080276,176.395808 L50.1028719,176.380341 C50.0100697,176.22567 49.9533572,176.040066 49.8863334,175.864773 C49.8193096,175.710103 49.7316631,175.565744 49.6904177,175.400762 L49.6904177,175.395606 C49.6388609,175.19969 49.6285496,174.993463 49.6079269,174.792392 C49.5873041,174.637722 49.5460587,174.483051 49.5460587,174.328381 L49.5460587,174.31807 L49.5460587,63.5689658 L25.5979377,49.7723734 L8.25939501,39.8012935 L8.25939501,39.7961379 Z M53.6809119,8.89300821 L12.3994039,32.6555248 L53.6706006,56.4180414 L94.9469529,32.6503692 L53.6706006,8.89300821 L53.6809119,8.89300821 Z M75.1491521,157.19091 L99.0972731,143.404629 L99.0972731,39.7961379 L81.7587304,49.7775291 L57.8054537,63.5689658 L57.8054537,167.177457 L75.1491521,157.19091 Z M202.324244,36.934737 L161.047891,60.6972536 L202.324244,84.4597702 L243.59544,60.6920979 L202.324244,36.934737 Z M198.194546,91.6106946 L174.24127,77.8192579 L156.902727,67.8378666 L156.902727,114.878266 L180.850848,128.664547 L198.194546,138.651094 L198.194546,91.6106946 Z M103.216659,197.616575 L163.759778,163.052915 L194.023603,145.781396 L152.778185,122.034346 L105.289242,149.374903 L62.0073307,174.292291 L103.216659,197.616575 Z" fill="#3f81f5"> </path> </g> </g></svg>
                 <h3 class="ml-4 text-2xl font-semibold">Laravel</h3>
             </div>
-            <p class="mt-4 text-gray-700 text-sm leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras suscipit arcu id turpis auctor, et vulputate justo efficitur. Integer volutpat, justo nec pharetra mollis.</p>
+            <p class="mt-4 text-gray-700 text-sm leading-relaxed">Building upon my strong foundational knowledge of PHP, the first framework I decided to learn was Laravel. It has increased the speed of my development by taking care of a lot of the menial tasks and allowing me to get into the nitty gritty.</p>
         </div>       
     </div>
     <div class="p-6 text-gray-900 flex-wrap flex justify-center items-center">
@@ -68,7 +68,7 @@
                     </svg>
                 <h3 class="ml-4 text-2xl font-semibold">C#</h3>
             </div>
-            <p class="mt-4 text-gray-700 text-sm leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras suscipit arcu id turpis auctor, et vulputate justo efficitur. Integer volutpat, justo nec pharetra mollis.</p>
+            <p class="mt-4 text-gray-700 text-sm leading-relaxed">I have developed my knowledge of C# by creating many applications harnasing fundamental concepts of the language, while also using text files to store information locally and retrieve it when needed.</p>
         </div>
                 <div class='max-w-[350px] m-5 rounded-lg'>
                     <div class="flex items-center">
@@ -77,15 +77,15 @@
                             </svg>
                         <h3 class="ml-4 text-2xl font-semibold">JQuery</h3>
                     </div>
-                    <p class="mt-4 text-gray-700 text-sm leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras suscipit arcu id turpis auctor, et vulputate justo efficitur. Integer volutpat, justo nec pharetra mollis.</p>
+                    <p class="mt-4 text-gray-700 text-sm leading-relaxed">JQuery is a recent interest, and has been used on the portfolio page on this website for its sort by feature.</p>
                 </div>
     
                 <div class='max-w-[350px] m-5 rounded-lg'>
                     <div class="flex items-center">
                         <svg viewBox="0 0 32 32" x="0px" y="0px" width="100" height="100" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><title>file_type_sql</title><path d="M8.562,15.256A21.159,21.159,0,0,0,16,16.449a21.159,21.159,0,0,0,7.438-1.194c1.864-.727,2.525-1.535,2.525-2V9.7a10.357,10.357,0,0,1-2.084,1.076A22.293,22.293,0,0,1,16,12.078a22.36,22.36,0,0,1-7.879-1.3A10.28,10.28,0,0,1,6.037,9.7v3.55C6.037,13.724,6.7,14.528,8.562,15.256Z" style="fill:#3f8cde"></path><path d="M8.562,21.961a15.611,15.611,0,0,0,2.6.741A24.9,24.9,0,0,0,16,23.155a24.9,24.9,0,0,0,4.838-.452,15.614,15.614,0,0,0,2.6-.741c1.864-.727,2.525-1.535,2.525-2v-3.39a10.706,10.706,0,0,1-1.692.825A23.49,23.49,0,0,1,16,18.74a23.49,23.49,0,0,1-8.271-1.348,10.829,10.829,0,0,1-1.692-.825V19.96C6.037,20.426,6.7,21.231,8.562,21.961Z" style="fill:#3f8cde"></path><path d="M16,30c5.5,0,9.963-1.744,9.963-3.894V23.269a10.5,10.5,0,0,1-1.535.762l-.157.063A23.487,23.487,0,0,1,16,25.445a23.422,23.422,0,0,1-8.271-1.351c-.054-.02-.106-.043-.157-.063a10.5,10.5,0,0,1-1.535-.762v2.837C6.037,28.256,10.5,30,16,30Z" style="fill:#3f8cde"></path><ellipse cx="16" cy="5.894" rx="9.963" ry="3.894" style="fill:#3f8cde"></ellipse></g></svg>
-                        <h3 class="ml-4 text-2xl font-semibold">SQL</h3>
+                        <h3 class="ml-4 text-2xl font-semibold">SQL/MySQL</h3>
                     </div>
-                    <p class="mt-4 text-gray-700 text-sm leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras suscipit arcu id turpis auctor, et vulputate justo efficitur. Integer volutpat, justo nec pharetra mollis.</p>
+                    <p class="mt-4 text-gray-700 text-sm leading-relaxed">I have been using MySQL, database relationships and SQL queries to communicate data to my project. Over time I have become more adept at this and in projects have been able to create a blog post system, which not only links the account information to the post but has likes connected to the post, each like exclusively being from different accounts.</p>
                 </div>
             </div>
         </div>
